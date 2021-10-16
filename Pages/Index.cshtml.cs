3876868756ab5a0c55c99a11522895a3b2042899
Jason Mandabrandja <jasonmandabrandja@gmail.com>
2021-10-16 18:20:46 +0000

    first commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace LogDemoApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            _logger.LogInformation("You requested the index page");
+            try
+            {
+                throw new Exception("Launch error for demo");
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError(ex, "J'ai un problème");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Configuration;
+using Serilog;
+
+namespace LogDemoApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+            Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration).CreateLogger();
+            try
+            {
+                Log.Information("Démarrage");
+                CreateHostBuilder(args).Build().Run();
+                throw new Exception("Jason Mandabrandja");
+            }
+            catch (Exception exp)
+            {
+                Log.Fatal(exp, "L'application ne démarre pas correctement");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
